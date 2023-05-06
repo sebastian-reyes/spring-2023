e9e49977bee1ae35613a0a2b590e253f7d67cc51
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2023-05-05 19:41:58 -0500

    SERV20230003 - registrarCategoria (POST)

--- src/main/java/com/example/app/rest/CategoriaRestController.java
+++ src/main/java/com/example/app/rest/CategoriaRestController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,14 +25,14 @@ public class CategoriaRestController {
         Map<String, Object> response = new HashMap<>();
         List<Categoria> categorias = service.listarCategorias();
         try {
-            if(categorias!=null){
-                response.put("categorias",categorias);
+            if (categorias != null) {
+                response.put("categorias", categorias);
                 return new ResponseEntity<>(response, HttpStatus.OK);
-            }else{
+            } else {
                 response.put("mensaje", "No se encontraron categorías");
                 return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
             }
-        }catch (DataAccessException e) {
+        } catch (DataAccessException e) {
             response.put("mensaje", "Error al realizar la consulta a la base de datos.");
             response.put("error", Objects.requireNonNull(e.getMessage()).concat(": ").concat(e.getMostSpecificCause().getMessage()));
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -49,11 +46,32 @@ public class CategoriaRestController {
         try {
             categoria = service.buscarCategoria(id);
             if (categoria != null) {
-                response.put("categoria",categoria);
+                response.put("categoria", categoria);
                 return new ResponseEntity<>(response, HttpStatus.OK);
-            }else{
+            } else {
                 response.put("mensaje", "La categoría no se encontró en la base de datos.");
                 return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+            }
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar la consulta a la base de datos.");
+            response.put("error", Objects.requireNonNull(e.getMessage()).concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registrarCategoria(@RequestBody Categoria categoria) {
+        Categoria nuevaCategoria = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            nuevaCategoria = service.guardarCategoria(categoria);
+            if (nuevaCategoria != null) {
+                response.put("categoria", nuevaCategoria);
+                response.put("mensaje", "La categoría fue creada correctamente.");
+                return new ResponseEntity<>(response, HttpStatus.CREATED);
+            } else {
+                response.put("mensaje", "Petición incorrecta");
+                return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
             }
         } catch (DataAccessException e) {
             response.put("mensaje", "Error al realizar la consulta a la base de datos.");
